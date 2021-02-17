3ca15be99457381a333061d10c4baffbd9369b1e
YiyuZhang <brianazhang@berkeley.edu>
2021-02-16 16:37:27 -0800

    complete hw03

--- hw03/expert_system.py
+++ hw03/expert_system.py
@@ -1,29 +1,29 @@
 def expertSystem():
-    coughing = input("Are you coughing:")
-    shortBreath = input("Are you short of breath or wheezing or coughing up phlegm:")
-    headache =input("Do you have a headache:")
-    pain = input("Are you experiencing any of the following: pain when bending your head forward,nausea or vomitting,bright light hurting your eyes, drowsiness or confusion:")
-    vomit = input("Are you vomitting or had diarrhea:")
-    aching =input("Do you have aching bones or aching joints:")
-    rash = input("Do you have a rash:")
-    soreThrout = (input("Do you have a sore throat:")
-    backPain =input("Do you have back pain just above the waist with chills and fever:")
-    urinatingPain =input("Do you have pain urinating or are urinating more often:")
-    sunCondition = input("Have you spent the day in the sun or in hot conditon:")
-    if coughing == 'yes' or 'Yes' or 'y' or'Y': 
-        if shortBreath =='yes' or 'Yes' or 'y' or'Y':
+    coughing = input("Are you coughing?(y/n):")
+    shortBreath = input("Are you short of breath or wheezing or coughing up phlegm?(y/n):")
+    headache =input("Do you have a headache?(y/n):")
+    pain = input("Are you experiencing any of the following: pain when bending your head forward,nausea or vomitting,bright light hurting your eyes, drowsiness or confusion?(y/n):")
+    vomit = input("Are you vomitting or had diarrhea?(y/n):")
+    aching =input("Do you have aching bones or aching joints?(y/n):")
+    rash = input("Do you have a rash?(y/n):")
+    soreThrout = input("Do you have a sore throat?(y/n):")
+    backPain =input("Do you have back pain just above the waist with chills and fever?(y/n):")
+    urinatingPain =input("Do you have pain urinating or are urinating more often?(y/n):")
+    sunCondition = input("Have you spent the day in the sun or in hot conditon?(y/n):")
+    if coughing == 'y': 
+        if shortBreath =='y':
             print("Possibilities include pneumonia or infection of airways")
         else:
-            if headache == 'yes' or 'Yes' or 'y' or'Y':
+            if headache == 'y':
                 print("Possibilities include viral infection")
             else:
-                if aching == 'yes' or 'Yes' or 'y' or'Y':
+                if aching == 'y':
                     print("Possibilities include viral infection")
                 else:
-                    if rash == 'yes' or 'Yes' or 'y' or'Y':
+                    if rash == 'y':
                         print("Insufficient information to list possibilities")
                     else:
-                        if soreThrout == 'yes' or 'Yes' or 'y' or'Y':
+                        if soreThrout == 'y':
                             print("Possibilities include a throat infection")
                         else:
                             if backPain:
@@ -34,11 +34,11 @@ def expertSystem():
                                 else: 
                                     print("Insufficient information to list possibilities")
     else:
-        if headache:
-            if pain:
+        if headache == 'y':
+            if pain == 'y':
                 print("Possibilities include menigitis")
             else:
-                if vomit:
+                if vomit == 'y':
                     print("Possibilities include digestive tract infection")
 
 expertSystem()
